af7786e20b65d1719cb996d670ed0cd8bb5d5ce7
mdrokun <rukonuddin623@gmail.com>
2019-06-27 23:52:53 +0600

    Finished the showcase section with modal form.

--- app/script.js
+++ app/script.js
@@ -1,27 +1,38 @@
-const signupModal = document.querySelector('.signup-form-wrapper'),
-    loginModal = document.querySelector('.login-form-wrapper'),
-    signupBtn = document.querySelector('.signup-btn'),
-    loginBtn = document.querySelector('.login-btn'),
-    signupX = document.querySelector('.signup-x'),
+const signinBtn = document.querySelector('.signin-btn'),
+    signinModal = document.querySelector('.login-form'),
     loginX = document.querySelector('.login-x'),
+    joinBtn = document.querySelector('.join-btn'),
+    joinModal = document.querySelector('.signup-form'),
+    signupX = document.querySelector('.signup-x'),
     formContainer = document.querySelector('.form-container')
 
-signupBtn.addEventListener('click', () => {
-    signupModal.classList.add('display')
+signinBtn.addEventListener('click', () => {
+    signinModal.classList.add('display-modal')
     formContainer.classList.add('disable')
 })
 
-loginBtn.addEventListener('click', () => {
-    loginModal.classList.add('display')
+loginX.addEventListener('click', () => {
+    signinModal.classList.remove('display-modal')
+    formContainer.classList.remove('disable')
+})
+
+joinBtn.addEventListener('click', () => {
+    joinModal.classList.add('display-modal')
     formContainer.classList.add('disable')
 })
 
 signupX.addEventListener('click', () => {
-    signupModal.classList.remove('display')
+    joinModal.classList.remove('display-modal')
     formContainer.classList.remove('disable')
 })
 
-loginX.addEventListener('click', () => {
-    loginModal.classList.remove('display')
-    formContainer.classList.remove('disable')
-})
+window.addEventListener("DOMContentLoaded", function (e) {
+
+    var stage = document.getElementById("stage");
+    var fadeComplete = function (e) { stage.appendChild(arr[0]); };
+    var arr = stage.getElementsByTagName("a");
+    for (var i = 0; i < arr.length; i++) {
+        arr[i].addEventListener("animationend", fadeComplete, false);
+    }
+
+}, false);
